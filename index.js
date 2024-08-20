4a07c0b669f3f4b79dde8960ed117d79be68bba7
Parker Mathis <parkerjmathis@gmail.com>
2024-08-19 19:02:40 -0700

    Install cron and express

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const { twitterClient } = require("./twitterClient.js");
+const CronJob = require("cron").CronJob;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 const tweet = async () => {
   try {
@@ -10,3 +18,10 @@ const tweet = async () => {
 };
 
 tweet();
+
+// example using cron to send every 30 seconds
+// const cronTweet = new CronJob("30 * * * * *", async () => {
+//   tweet();
+// });
+
+// cronTweet.start();
